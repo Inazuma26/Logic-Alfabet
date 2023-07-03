8d82b0f1fefcef4f753297d31cef9cb1355a3839
Inazuma26 <136417398+Inazuma26@users.noreply.github.com>
2023-07-03 21:11:15 +0700

    Add files via upload

---
+++ nama.php
@@ -0,0 +1,61 @@
+<?php
+for($y = 1;$y <10;$y++){
+for($x = 1;$x <10;$x++){
+if($x==1 || $y==5 || $x==9){
+echo"* ";
+}
+else{echo"  ";}
+}
+echo PHP_EOL;
+}
+echo PHP_EOL;
+echo PHP_EOL;
+
+for($y = 1;$y <10;$y++){
+for($x = 1;$x <10;$x++){
+if($x==1 || $y==1 || $y==5 || $x==9){
+echo"* ";
+}
+else{echo"  ";}
+}
+echo PHP_EOL;
+}
+echo PHP_EOL;
+echo PHP_EOL;
+
+for($y = 1;$y <10;$y++){
+for($x = 1;$x <10;$x++){
+if($x==1 || $y==$x || $x==9){
+echo"* ";
+}
+else{echo"  ";}
+}
+echo PHP_EOL;
+}
+echo PHP_EOL;
+echo PHP_EOL;
+
+for($y = 1;$y <10;$y++){
+for($x = 1;$x <10;$x++){
+if($x==5){
+echo"* ";
+}
+else{echo"  ";}
+}
+echo PHP_EOL;
+}
+echo PHP_EOL;
+echo PHP_EOL;
+
+for($y = 1;$y <10;$y++){
+for($x = 1;$x <10;$x++){
+if($x==1 || $y==5 || $y==1){
+echo"* ";
+}
+else{echo"  ";}
+}
+echo PHP_EOL;
+}
+?>
+
+
